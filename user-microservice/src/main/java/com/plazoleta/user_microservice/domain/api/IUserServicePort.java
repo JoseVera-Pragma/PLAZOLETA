9db48427d184baa75f93c7fd70d0b94691d3a657
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-23 20:46:39 -0500

    refactor: Update IUserServicePort to include Role parameter in saveUser and updateUser methods

--- user-microservice/src/main/java/com/plazoleta/user_microservice/domain/api/IUserServicePort.java
+++ user-microservice/src/main/java/com/plazoleta/user_microservice/domain/api/IUserServicePort.java
@@ -1,13 +1,14 @@
 package com.plazoleta.user_microservice.domain.api;
 
 import com.plazoleta.user_microservice.domain.model.Email;
+import com.plazoleta.user_microservice.domain.model.Role;
 import com.plazoleta.user_microservice.domain.model.User;
 
 import java.util.List;
 
 public interface IUserServicePort {
 
-    User saveUser(User user);
+    User saveUser(User user, Role creatorRole);
 
     User getUser(Long id);
 
@@ -15,7 +16,7 @@ public interface IUserServicePort {
 
     List<User> getAllUsers();
 
-    void updateUser(User user);
+    void updateUser(User user, Role creatorRole);
 
     void deleteUser(Long id);
 }
