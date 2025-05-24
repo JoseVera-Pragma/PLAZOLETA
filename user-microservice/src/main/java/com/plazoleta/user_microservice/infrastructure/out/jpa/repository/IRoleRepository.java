5e9fe77c15f341dc7e033cdfa749e11396361046
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-24 00:19:48 -0500

    refactor: Change return type of findByName in IRoleRepository and findByEmail in IUserRepository to improve consistency

--- user-microservice/src/main/java/com/plazoleta/user_microservice/infrastructure/out/jpa/repository/IRoleRepository.java
+++ user-microservice/src/main/java/com/plazoleta/user_microservice/infrastructure/out/jpa/repository/IRoleRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface IRoleRepository extends JpaRepository <RoleEntity, Long> {
-    Optional<RoleEntity> findByName(RoleList name);
+    RoleEntity findByName(RoleList name);
     boolean existsByName(String name);
 }
--- user-microservice/src/main/java/com/plazoleta/user_microservice/infrastructure/out/jpa/repository/IUserRepository.java
+++ user-microservice/src/main/java/com/plazoleta/user_microservice/infrastructure/out/jpa/repository/IUserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface IUserRepository extends JpaRepository<UserEntity, Long> {
-    Optional<UserEntity> findByEmail(String email);
+    UserEntity findByEmail(String email);
     boolean existsByEmail(String email);
     boolean existsByRoleId(Long roleId);
 }
