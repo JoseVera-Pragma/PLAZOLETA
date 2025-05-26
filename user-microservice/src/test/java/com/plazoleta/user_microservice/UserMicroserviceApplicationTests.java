5d3a4bbd25fd46b05292cf5ac114b422f65637b4
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-26 17:46:58 -0500

    feat: Update JaCoCo coverage exclusions and refine test configuration for UserMicroserviceApplicationTests

--- user-microservice/src/test/java/com/plazoleta/user_microservice/UserMicroserviceApplicationTests.java
+++ user-microservice/src/test/java/com/plazoleta/user_microservice/UserMicroserviceApplicationTests.java
@@ -1,11 +1,9 @@
 package com.plazoleta.user_microservice;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-@Disabled
+@SpringBootTest(properties = "spring.profiles.active=test")
 class UserMicroserviceApplicationTests {
 
 	@Test
