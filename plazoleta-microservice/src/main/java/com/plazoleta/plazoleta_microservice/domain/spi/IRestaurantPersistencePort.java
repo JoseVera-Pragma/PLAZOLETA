831ae5e4e8a414914f671a80cb212611f406080d
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-26 22:03:42 -0500

    feat: Add IRestaurantPersistencePort and IUserSecurityPort interfaces
    - IRestaurantPersistencePort defines persistence operations for restaurants.
    - IUserSecurityPort abstracts user retrieval via Feign client to enable user role validation.

---
+++ plazoleta-microservice/src/main/java/com/plazoleta/plazoleta_microservice/domain/spi/IRestaurantPersistencePort.java
@@ -0,0 +1,7 @@
+package com.plazoleta.plazoleta_microservice.domain.spi;
+
+import com.plazoleta.plazoleta_microservice.domain.model.Restaurant;
+
+public interface IRestaurantPersistencePort {
+    void saveRestaurant(Restaurant restaurant);
+}
