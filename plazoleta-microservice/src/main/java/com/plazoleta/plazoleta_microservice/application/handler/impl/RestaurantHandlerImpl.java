d36695e4fc172ec394093f4d03fde1f6f9d277f1
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-27 01:02:46 -0500

    feat: Implement RestaurantHandlerImpl for restaurant creation logic

---
+++ plazoleta-microservice/src/main/java/com/plazoleta/plazoleta_microservice/application/handler/impl/RestaurantHandlerImpl.java
@@ -0,0 +1,35 @@
+package com.plazoleta.plazoleta_microservice.application.handler.impl;
+
+import com.plazoleta.plazoleta_microservice.application.dto.request.RestaurantRequestDto;
+import com.plazoleta.plazoleta_microservice.application.dto.response.RestaurantResponseDto;
+import com.plazoleta.plazoleta_microservice.application.handler.IRestaurantHandler;
+import com.plazoleta.plazoleta_microservice.application.mapper.IRestaurantRequestMapper;
+import com.plazoleta.plazoleta_microservice.application.mapper.IRestaurantResponseMapper;
+import com.plazoleta.plazoleta_microservice.domain.api.IRestaurantServicePort;
+import com.plazoleta.plazoleta_microservice.domain.model.Restaurant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class RestaurantHandlerImpl implements IRestaurantHandler {
+
+    private final IRestaurantServicePort restaurantServicePort;
+    private final IRestaurantRequestMapper restaurantRequestMapper;
+    private final IRestaurantResponseMapper restaurantResponseMapper;
+
+    @Override
+    public RestaurantResponseDto createRestaurant(RestaurantRequestDto restaurantRequestDto) {
+
+        System.out.println("Inicio");
+        Restaurant restaurant = restaurantRequestMapper.toRestaurant(restaurantRequestDto);
+        System.out.println("2");
+
+        restaurantServicePort.createRestaurant(restaurant);
+        System.out.println("3");
+
+        return restaurantResponseMapper.toRestaurantResponseDto(restaurant);
+    }
+}
