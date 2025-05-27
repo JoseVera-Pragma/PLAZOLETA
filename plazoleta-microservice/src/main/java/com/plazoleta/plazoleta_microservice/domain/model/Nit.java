577b38cde32df05f803a0b80b554a422af9b95ae
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-26 22:01:47 -0500

    feat: Implement Nit, PhoneNumber, and Restaurant classes with validation and builder pattern

---
+++ plazoleta-microservice/src/main/java/com/plazoleta/plazoleta_microservice/domain/model/Nit.java
@@ -0,0 +1,36 @@
+package com.plazoleta.plazoleta_microservice.domain.model;
+
+import com.plazoleta.plazoleta_microservice.domain.exception.InvalidNitException;
+import com.plazoleta.plazoleta_microservice.domain.exception.MissingNitException;
+
+import java.util.Objects;
+
+public class Nit {
+    private final String value;
+
+    public Nit(String value) {
+        if (value == null || value.isBlank()) {
+            throw new MissingNitException("Nit must be provided");
+        } else if (!value.matches("^\\d{1,13}$")) {
+            throw new InvalidNitException("Nit is not valid: " + value);
+        }
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Nit that = (Nit) o;
+        return value.equals(that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
