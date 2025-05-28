ed0647dfcb1d71d27410e40035981515373f917b
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-27 20:10:48 -0500

    refactor: Remove unused UserResponse class and update .gitignore for test reports

--- plazoleta-microservice/src/main/java/com/plazoleta/plazoleta_microservice/infrastructure/out/feing/UserResponse.java
+++
@@ -1,7 +0,0 @@
-package com.plazoleta.plazoleta_microservice.infrastructure.out.feing;
-
-import com.plazoleta.plazoleta_microservice.domain.model.User;
-
-public class UserResponse extends User {
-
-}
