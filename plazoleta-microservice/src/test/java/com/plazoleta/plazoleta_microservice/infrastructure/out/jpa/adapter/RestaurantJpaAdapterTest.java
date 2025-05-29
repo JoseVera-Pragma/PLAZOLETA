15582fb1333f2d036888be423141c365e4ca1b84
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-28 19:11:55 -0500

    test: add integration tests for RestaurantJpaAdapter and mapper tests for IRestaurantEntityMapper

--- plazoleta-microservice/src/test/java/com/plazoleta/plazoleta_microservice/infrastructure/out/jpa/adapter/RestaurantJpaAdapterTest.java
+++ plazoleta-microservice/src/test/java/com/plazoleta/plazoleta_microservice/infrastructure/out/jpa/adapter/RestaurantJpaAdapterTest.java
@@ -1,6 +1,7 @@
 package com.plazoleta.plazoleta_microservice.infrastructure.out.jpa.adapter;
 
 import com.plazoleta.plazoleta_microservice.domain.model.Restaurant;
+import com.plazoleta.plazoleta_microservice.infrastructure.exception.RestaurantNotFoundException;
 import com.plazoleta.plazoleta_microservice.infrastructure.out.jpa.entity.RestaurantEntity;
 import com.plazoleta.plazoleta_microservice.infrastructure.out.jpa.mapper.IRestaurantEntityMapper;
 import com.plazoleta.plazoleta_microservice.infrastructure.out.jpa.repository.IRestaurantRepository;
@@ -9,11 +10,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-
 class RestaurantJpaAdapterTest {
     private IRestaurantRepository restaurantRepository;
     private IRestaurantEntityMapper restaurantEntityMapper;
@@ -58,4 +60,38 @@ class RestaurantJpaAdapterTest {
         assert(exists);
     }
 
+    @Test
+    void testgetById() {
+        Restaurant restaurant = new Restaurant.Builder()
+                .id(1L)
+                .name("Name")
+                .nit("123456")
+                .address("Address")
+                .phoneNumber("+123456789")
+                .urlLogo("http://logo.url")
+                .idOwner(1L)
+                .build();
+        RestaurantEntity restaurantEntity = new RestaurantEntity(1L,"Name","123456","Address","+123456789","http://logo.url",1L);
+
+        when(restaurantRepository.findById(1L)).thenReturn(Optional.of(restaurantEntity));
+        when(restaurantEntityMapper.toRestaurant(restaurantEntity)).thenReturn(restaurant);
+
+        Restaurant result = adapter.getById(1L);
+
+        assertNotNull(result);
+        assertEquals(restaurant.getId(), result.getId());
+        assertEquals(restaurant.getName(), result.getName());
+        verify(restaurantRepository).findById(1L);
+        verify(restaurantEntityMapper).toRestaurant(restaurantEntity);
+    }
+
+    @Test
+    void getById_ShouldThrowException_WhenRestaurantDoesNotExist() {
+        Long restaurantId = 99L;
+        when(restaurantRepository.findById(restaurantId)).thenReturn(Optional.empty());
+
+        assertThrows(RestaurantNotFoundException.class, () -> adapter.getById(restaurantId));
+        verify(restaurantRepository).findById(restaurantId);
+        verifyNoInteractions(restaurantEntityMapper);
+    }
 }
