702e98b1924423cfde334654fcdb7180892c9bd2
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-28 21:42:16 -0500

    refactor: remove DishAlreadyExistsException class as it is no longer needed

--- plazoleta-microservice/src/main/java/com/plazoleta/plazoleta_microservice/domain/exception/dish/DishAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.plazoleta.plazoleta_microservice.domain.exception.dish;
-
-import com.plazoleta.plazoleta_microservice.domain.exception.DomainException;
-
-public class DishAlreadyExistsException extends DomainException {
-    public DishAlreadyExistsException(String message) {
-        super(message);
-    }
-}
