1763541384344495e8a1d9c45c677e80280b3286
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-28 21:43:51 -0500

    feat: implement updateDish and getById methods in DishJpaAdapter for dish management

--- plazoleta-microservice/src/main/java/com/plazoleta/plazoleta_microservice/infrastructure/out/jpa/adapter/DishJpaAdapter.java
+++ plazoleta-microservice/src/main/java/com/plazoleta/plazoleta_microservice/infrastructure/out/jpa/adapter/DishJpaAdapter.java
@@ -1,5 +1,6 @@
 package com.plazoleta.plazoleta_microservice.infrastructure.out.jpa.adapter;
 
+import com.plazoleta.plazoleta_microservice.domain.exception.dish.DishNotFoundException;
 import com.plazoleta.plazoleta_microservice.domain.model.Dish;
 import com.plazoleta.plazoleta_microservice.domain.spi.IDishPersistencePort;
 import com.plazoleta.plazoleta_microservice.infrastructure.out.jpa.mapper.IDishEntityMapper;
@@ -16,6 +17,18 @@ public class DishJpaAdapter implements IDishPersistencePort {
     @Override
     public Dish saveDish(Dish dish) {
         return dishEntityMapper.toModel(dishRepository.save(dishEntityMapper.toEntity(dish)));
+    }
+
+    @Override
+    public void updateDish(Dish dish) {
+        dishRepository.save(dishEntityMapper.toEntity(dish));
+    }
+
+    @Override
+    public Dish getById(Long id) {
+        return dishRepository.findById(id)
+                .map(dishEntityMapper::toModel)
+                .orElseThrow(() -> new DishNotFoundException("Dish not found with id: " + id));
     }
 
 }
