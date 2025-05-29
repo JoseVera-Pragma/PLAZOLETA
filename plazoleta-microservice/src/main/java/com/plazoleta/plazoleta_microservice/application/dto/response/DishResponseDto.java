739ad9173847c6ac9d2065674e551b48e68c9e31
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-28 19:08:09 -0500

    feat: add constructors to CategoryResponseDto and DishResponseDto for improved object creation

--- plazoleta-microservice/src/main/java/com/plazoleta/plazoleta_microservice/application/dto/response/DishResponseDto.java
+++ plazoleta-microservice/src/main/java/com/plazoleta/plazoleta_microservice/application/dto/response/DishResponseDto.java
@@ -1,12 +1,12 @@
 package com.plazoleta.plazoleta_microservice.application.dto.response;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class DishResponseDto {
     private Long id;
     private String name;
