cdfa727b261f988447ebb5a5e5827f14743a5aa1
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-30 15:52:26 -0500

    feat: add JwtTokenAdapter for JWT token generation and validation; add PasswordEncoderAdapter for password encoding

---
+++ user-microservice/src/main/java/com/plazoleta/user_microservice/infrastructure/configuration/security/adapter/PasswordEncoderAdapter.java
@@ -0,0 +1,21 @@
+package com.plazoleta.user_microservice.infrastructure.configuration.security.adapter;
+
+import com.plazoleta.user_microservice.domain.spi.IPasswordEncoderPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RequiredArgsConstructor
+public class PasswordEncoderAdapter implements IPasswordEncoderPort {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public String encode(String rawPassword) {
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    @Override
+    public boolean matches(String rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
