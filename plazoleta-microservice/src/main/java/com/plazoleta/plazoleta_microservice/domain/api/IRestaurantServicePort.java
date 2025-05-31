a784c5cc6e812eea3034e66a1a9d1ba1fd12f810
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-31 15:48:01 -0500

    feat: add findAll method to IRestaurantServicePort and IRestaurantPersistencePort for paginated restaurant retrieval

--- plazoleta-microservice/src/main/java/com/plazoleta/plazoleta_microservice/domain/api/IRestaurantServicePort.java
+++ plazoleta-microservice/src/main/java/com/plazoleta/plazoleta_microservice/domain/api/IRestaurantServicePort.java
@@ -1,7 +1,10 @@
 package com.plazoleta.plazoleta_microservice.domain.api;
 
 import com.plazoleta.plazoleta_microservice.domain.model.Restaurant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface IRestaurantServicePort {
     void createRestaurant(Restaurant restaurant);
+    Page<Restaurant> findAll(Pageable pageable);
 }
