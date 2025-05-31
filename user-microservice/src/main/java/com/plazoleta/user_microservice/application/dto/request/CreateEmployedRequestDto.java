98f48c3e413bc361afaf44681ad5fdb553c04903
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-31 13:42:57 -0500

    feat: create DTOs for user registration including basic, employed, and owner details

--- user-microservice/src/main/java/com/plazoleta/user_microservice/application/dto/request/CreateEmployedRequestDto.java
+++ user-microservice/src/main/java/com/plazoleta/user_microservice/application/dto/request/CreateEmployedRequestDto.java
@@ -1,36 +1,12 @@
 package com.plazoleta.user_microservice.application.dto.request;
 
-import jakarta.validation.constraints.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDate;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class CreateEmployedRequestDto {
-
-    @NotBlank(message = "First name is required")
-    private String firstName;
-
-    @NotBlank(message = "Last name is required")
-    private String lastName;
-
-    @NotBlank(message = "Identity number is required")
-    @Pattern(regexp = "^\\d+$", message = "Identity number must contain only digits")
-    private String identityNumber;
-
-    @NotBlank(message = "Phone number is required")
-    @Size(max = 13, message = "Phone number is extensive max 13 characters")
-    @Pattern(regexp = "^\\+?\\d+$", message = "Phone number is not valid")
-    private String phoneNumber;
-
-    @NotBlank(message = "Email is required")
-    @Email(message = "Email is not valid")
-    private String email;
-
-    @NotBlank(message = "Password is required")
-    private String password;
+@SuperBuilder
+public class CreateEmployedRequestDto extends CreateBasicUserRequestDto {
 }
