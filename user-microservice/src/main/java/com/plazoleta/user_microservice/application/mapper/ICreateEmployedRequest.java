ecfb30c5bbba319804acd309fd1de753ee0ffadb
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-30 20:06:43 -0500

    feat: implement ICreateEmployedRequest mapper for CreateEmployedRequestDto to User conversion

---
+++ user-microservice/src/main/java/com/plazoleta/user_microservice/application/mapper/ICreateEmployedRequest.java
@@ -0,0 +1,39 @@
+package com.plazoleta.user_microservice.application.mapper;
+
+import com.plazoleta.user_microservice.application.dto.request.CreateEmployedRequestDto;
+import com.plazoleta.user_microservice.domain.model.Email;
+import com.plazoleta.user_microservice.domain.model.IdentityNumber;
+import com.plazoleta.user_microservice.domain.model.PhoneNumber;
+import com.plazoleta.user_microservice.domain.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE,
+        uses = {ValueObjectMapper.class})
+public interface ICreateEmployedRequest {
+
+    @Mapping(target = "email", source = "email", qualifiedByName = "toEmail")
+    @Mapping(target = "identityNumber", source = "identityNumber", qualifiedByName = "toIdentityNumber")
+    @Mapping(target = "phoneNumber", source = "phoneNumber", qualifiedByName = "toPhoneNumber")
+    User toUser(CreateEmployedRequestDto createEmployedRequestDto);
+
+    @Named("toEmail")
+    default Email toEmail(String email) {
+        return new Email(email);
+    }
+
+    @Named("toIdentityNumber")
+    default IdentityNumber toIdentityNumber(String identityNumber) {
+        return new IdentityNumber(identityNumber);
+    }
+
+    @Named("toPhoneNumber")
+    default PhoneNumber toPhoneNumber(String phoneNumber) {
+        return new PhoneNumber(phoneNumber);
+    }
+
+}
