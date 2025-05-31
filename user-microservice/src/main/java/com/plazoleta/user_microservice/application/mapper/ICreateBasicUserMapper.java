3d33121c5c1e0ec24542e055aced6fce19fb148b
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-05-31 13:43:04 -0500

    feat: implement ICreateBasicUserMapper for mapping CreateBasicUserRequestDto to User

--- user-microservice/src/main/java/com/plazoleta/user_microservice/application/mapper/ICreateBasicUserMapper.java
+++ user-microservice/src/main/java/com/plazoleta/user_microservice/application/mapper/ICreateBasicUserMapper.java
@@ -1,22 +1,25 @@
 package com.plazoleta.user_microservice.application.mapper;
 
-import com.plazoleta.user_microservice.application.dto.request.CreateOwnerRequestDto;
-import com.plazoleta.user_microservice.domain.model.*;
+import com.plazoleta.user_microservice.application.dto.request.CreateBasicUserRequestDto;
+import com.plazoleta.user_microservice.domain.model.Email;
+import com.plazoleta.user_microservice.domain.model.IdentityNumber;
+import com.plazoleta.user_microservice.domain.model.PhoneNumber;
+import com.plazoleta.user_microservice.domain.model.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring",
-    unmappedTargetPolicy = ReportingPolicy.IGNORE,
-    unmappedSourcePolicy = ReportingPolicy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE,
         uses = {ValueObjectMapper.class})
-public interface IUserRequestMapper {
+public interface ICreateBasicUserMapper {
 
     @Mapping(target = "email", source = "email", qualifiedByName = "toEmail")
     @Mapping(target = "identityNumber", source = "identityNumber", qualifiedByName = "toIdentityNumber")
     @Mapping(target = "phoneNumber", source = "phoneNumber", qualifiedByName = "toPhoneNumber")
-    User toUser(CreateOwnerRequestDto createOwnerRequestDto);
+    User toUser(CreateBasicUserRequestDto createBasicUserRequestDto);
 
     @Named("toEmail")
     default Email toEmail(String email) {
@@ -27,6 +30,7 @@ public interface IUserRequestMapper {
     default IdentityNumber toIdentityNumber(String identityNumber) {
         return new IdentityNumber(identityNumber);
     }
+
     @Named("toPhoneNumber")
     default PhoneNumber toPhoneNumber(String phoneNumber) {
         return new PhoneNumber(phoneNumber);
