273f199130eeb187cac0d6566cb078f144a4d1dd
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-06-07 12:21:31 -0500

    feat: Integrate dotenv for JWT secret management and update application.yml in both microservices

--- plazoleta-microservice/src/main/java/com/plazoleta/plazoleta_microservice/PlazoletaMicroserviceApplication.java
+++ plazoleta-microservice/src/main/java/com/plazoleta/plazoleta_microservice/PlazoletaMicroserviceApplication.java
@@ -1,5 +1,6 @@
 package com.plazoleta.plazoleta_microservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,6 +10,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class PlazoletaMicroserviceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+
 		SpringApplication.run(PlazoletaMicroserviceApplication.class, args);
 	}
 
