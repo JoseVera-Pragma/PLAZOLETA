81115208c023cdc10adb9d1ad2674950e4bd8aa9
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-06-07 12:06:07 -0500

    feat: Integrate dotenv for environment variable management and update application.yml for Twilio configuration

--- sms-service/src/main/java/com/plazoleta/sms_service/SmsServiceApplication.java
+++ sms-service/src/main/java/com/plazoleta/sms_service/SmsServiceApplication.java
@@ -2,11 +2,18 @@ package com.plazoleta.sms_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class SmsServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("TWILIO_ACCOUNT_SID", dotenv.get("TWILIO_ACCOUNT_SID"));
+		System.setProperty("TWILIO_AUTH_TOKEN", dotenv.get("TWILIO_AUTH_TOKEN"));
+		System.setProperty("TWILIO_PHONE_NUMBER", dotenv.get("TWILIO_PHONE_NUMBER"));
+
 		SpringApplication.run(SmsServiceApplication.class, args);
 	}
 
