456215aeb86c9d29c72834f65afaae71952cd35a
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-06-09 18:07:34 -0500

    refactor: Remove unused Feign clients and related exception classes

--- plazoleta-microservice/src/main/java/com/plazoleta/plazoleta_microservice/infrastructure/exception/CustomerHasActiveOrderException.java
+++
@@ -1,7 +0,0 @@
-package com.plazoleta.plazoleta_microservice.infrastructure.exception;
-
-public class CustomerHasActiveOrderException extends RuntimeException {
-    public CustomerHasActiveOrderException(Long customerId) {
-        super("Customer with ID " + customerId + " already has an order in process.");
-    }
-}
