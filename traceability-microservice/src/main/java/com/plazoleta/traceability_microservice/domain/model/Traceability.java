1cc61904ee48146941caf25ceed1e34c24afe2fe
Jose Hernando Vera Jaimes <jose.vera@pragma.com.co>
2025-06-09 18:08:56 -0500

    feat: Add Traceability model with Builder pattern and methods for state management

---
+++ traceability-microservice/src/main/java/com/plazoleta/traceability_microservice/domain/model/Traceability.java
@@ -0,0 +1,142 @@
+package com.plazoleta.traceability_microservice.domain.model;
+
+import java.time.LocalDateTime;
+
+public class Traceability {
+    private final String id;
+    private final Long orderId;
+    private final Long customerId;
+    private final String customerEmail;
+    private final LocalDateTime date;
+    private final String previousState;
+    private final String newState;
+    private final Long employedId;
+    private final String employedEmail;
+
+    private Traceability(Builder builder) {
+        this.id = builder.id;
+        this.orderId = builder.orderId;
+        this.customerId = builder.customerId;
+        this.customerEmail = builder.customerEmail;
+        this.date = builder.date;
+        this.previousState = builder.previousState;
+        this.newState = builder.newState;
+        this.employedId = builder.employedId;
+        this.employedEmail = builder.employedEmail;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String id;
+        private Long orderId;
+        private Long customerId;
+        private String customerEmail;
+        private LocalDateTime date;
+        private String previousState;
+        private String newState;
+        private Long employedId;
+        private String employedEmail;
+
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder orderId(Long orderId) {
+            this.orderId = orderId;
+            return this;
+        }
+
+        public Builder customerId(Long customerId) {
+            this.customerId = customerId;
+            return this;
+        }
+
+        public Builder customerEmail(String customerEmail) {
+            this.customerEmail = customerEmail;
+            return this;
+        }
+
+        public Builder date(LocalDateTime date) {
+            this.date = date;
+            return this;
+        }
+
+        public Builder previousState(String previousState) {
+            this.previousState = previousState;
+            return this;
+        }
+
+        public Builder newState(String newState) {
+            this.newState = newState;
+            return this;
+        }
+
+        public Builder employedId(Long employedId) {
+            this.employedId = employedId;
+            return this;
+        }
+
+        public Builder employedEmail(String employedEmail) {
+            this.employedEmail = employedEmail;
+            return this;
+        }
+
+        public Traceability build() {
+            return new Traceability(this);
+        }
+    }
+
+    public Traceability whitDate(LocalDateTime date) {
+        return Traceability.builder()
+                .id(this.id)
+                .orderId(this.orderId)
+                .customerId(this.customerId)
+                .customerEmail(this.customerEmail)
+                .date(date)
+                .previousState(this.previousState)
+                .newState(this.newState)
+                .employedId(this.employedId)
+                .employedEmail(this.employedEmail)
+                .build();
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public String getEmployedEmail() {
+        return employedEmail;
+    }
+
+    public Long getEmployedId() {
+        return employedId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNewState() {
+        return newState;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getPreviousState() {
+        return previousState;
+    }
+}
